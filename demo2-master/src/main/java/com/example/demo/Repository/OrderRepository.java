771bfd1b6a5b9d7a6daf49898f67c23fb915cd90
Administrator <2179715131@qq.com>
2026-01-05 16:39:11 +0800

    1

--- demo2-master/src/main/java/com/example/demo/Repository/OrderRepository.java
+++ demo2-master/src/main/java/com/example/demo/Repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.Repository;
 
 import com.example.demo.entity.cakeTable.Order;
-import com.example.demo.entity.cakeTable.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;

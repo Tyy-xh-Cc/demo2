6885b03d9c44c09c4422c2c10876ac25717e158f
Administrator <2179715131@qq.com>
2026-01-09 14:37:57 +0800

    new

--- src/main/java/com/example/demo/Controller/ProductController.java
+++ src/main/java/com/example/demo/Controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ProductController {
     
     private final ProductService productService;
--- src/main/java/com/example/demo/Controller/RestaurantController.java
+++ src/main/java/com/example/demo/Controller/RestaurantController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class RestaurantController {
     
     private final RestaurantService restaurantService;
